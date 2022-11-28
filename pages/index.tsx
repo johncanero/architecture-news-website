c43b62f8c1ae58015b593831c7da64653b772a5f
John Canero <johncanero.official@gmail.com>
2022-11-28 12:57:28 +0800

    Add a Heading Tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <h1 className=" text-blue-500 text-4xl font-bold">Hello World</h1>
+      <h1 className=" text-yellow-500 text-4xl font-bold">This is an Architecture Website</h1>
     </div>
   );
 }

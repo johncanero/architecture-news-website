7e8385f9e27b91540de397850956ec8df2638343
John Canero <johncanero.official@gmail.com>
2022-11-29 07:45:55 +0800

    Apply Dark Mode Feature and Fix Width and Header Margin and Padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Hero from "../sections/Hero";
+
+
 
 export default function Home() {
   return (
-    <div className=" font-Poppins">
-      <h1 className=" text-blue-500 text-4xl font-bold">Hello World</h1>
-      <h1 className=" text-yellow-500 text-4xl font-bold">This is an Architecture Website</h1>
+    <div>
+      {/* Main Content */}
+      <Hero />
     </div>
-  );
+  )
 }
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      {/* <h1 className="text-4xl font-bold text-blue-500 ">Hello World</h1>
+      <h1 className="text-4xl font-bold text-yellow-500 ">
+        This is an Architecture Website
+      </h1> */}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;

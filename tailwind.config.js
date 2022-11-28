a022edc6a0399a5a1c92f4f5687cf9544288d0c7
John Canero <johncanero.official@gmail.com>
2022-11-28 12:58:41 +0800

    Tailwind CSS Config Add

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
+    screens: {
+      sm: '320px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
+    fontFamily: {
+      Poppins: ['Poppins', 'sans-serif'],
+    },
+ 
   },
   plugins: [],
 }

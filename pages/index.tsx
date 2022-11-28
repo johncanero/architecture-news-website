e7fc96dd7c1ae0ab87d4a6b2532f0096a36ffbe9
John Canero <johncanero.official@gmail.com>
2022-11-28 13:31:26 +0800

    Application of Font Poppins

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
+    <div className=" font-Poppins">
       <h1 className=" text-blue-500 text-4xl font-bold">Hello World</h1>
       <h1 className=" text-yellow-500 text-4xl font-bold">This is an Architecture Website</h1>
     </div>

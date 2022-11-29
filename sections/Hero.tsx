a877b0054e7891859d6aa83908608b6ac629d792
John Canero <johncanero.official@gmail.com>
2022-11-29 17:21:24 +0800

    Apply Grid and Row Columns

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,10 +3,56 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      {/* <h1 className="text-4xl font-bold text-blue-500 ">Hello World</h1>
+      <div className="grid grid-cols-3 grid-rows-2 gap-4">
+        <div className="col-span-2 ">
+          {" "}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
+            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
+            ipsum explicabo beatae culpa.
+          </p>
+        </div>
+        <div className="row-span-2 ">
+          {" "}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
+            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
+            ipsum explicabo beatae culpa. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Eum possimus autem repudiandae reiciendis, esse
+            corporis! Beatae nihil maxime ipsam! Eius autem quidem animi facilis
+            deleniti pariatur ipsum explicabo beatae culpa.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
+            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
+            ipsum explicabo beatae culpa.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
+            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
+            ipsum explicabo beatae culpa.
+          </p>
+        </div>
+        <div>
+          {" "}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
+            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
+            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
+            ipsum explicabo beatae culpa.
+          </p>
+        </div>
+      </div>
+
+      <h1 className="text-4xl font-bold text-blue-500 ">Hello World</h1>
       <h1 className="text-4xl font-bold text-yellow-500 ">
         This is an Architecture Website
-      </h1> */}
+      </h1>
     </div>
   );
 };

0a34b48bf44ed3ce3720692ac1b01f391309e56d
John Canero <johncanero.official@gmail.com>
2022-11-29 23:01:16 +0800

    Style Hero HomePage

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,38 +1,39 @@
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <div>
-      <div className="grid grid-cols-3 grid-rows-2 gap-4">
+      <div className="grid grid-cols-3 grid-rows-2 gap-4 mt-8 font-Poppins">
         <div className="col-span-2 ">
           {" "}
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
-            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
-            ipsum explicabo beatae culpa.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
-            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
-            ipsum explicabo beatae culpa.
-            
-          </p>
+          <Image
+            src="/images/mactanAirport.jpg"
+            width={1000}
+            height={1000}
+            alt="aLogo"
+            className=" rounded-3xl"
+          />
         </div>
-        <div className="row-span-2 ">
-          {" "}
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
-            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
-            ipsum explicabo beatae culpa. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Eum possimus autem repudiandae reiciendis, esse
-            corporis! Beatae nihil maxime ipsam! Eius autem quidem animi facilis
-            deleniti pariatur ipsum explicabo beatae culpa.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-            possimus autem repudiandae reiciendis, esse corporis! Beatae nihil
-            maxime ipsam! Eius autem quidem animi facilis deleniti pariatur
-            ipsum explicabo beatae culpa.
-          </p>
+        <div className="row-span-2 text-white bg-gray-900 ">
+          <h1 className="mt-12 text-4xl font-semibold m-7 text-amber-400">New</h1>{" "}
+          <div className="m-7">
+            <h2 className="text-xl font-semibold">Philippine Architecture</h2>
+            <p className="mt-2 text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eum, aliquid, omnis laboriosam nemo corrupti eos velit necessitatibus cum, placeat ad ex maxime error? Quidem veritatis maxime optio tempore perspiciatis?</p>
+            <hr className="h-px my-6 bg-gray-700 border-0 dark:bg-gray-700"></hr>
+          </div>
+
+          <div className="m-7">
+            <h2 className="text-xl font-semibold">Philippine Architecture</h2>
+            <p className="mt-2 text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eum, aliquid, omnis laboriosam nemo corrupti eos velit necessitatibus cum, placeat ad ex maxime error? Quidem veritatis maxime optio tempore perspiciatis?</p>
+            <hr className="h-px my-6 bg-gray-700 border-0 dark:bg-gray-700"></hr>
+          </div>
+
+          <div className="m-7">
+            <h2 className="text-xl font-semibold">Philippine Architecture</h2>
+            <p className="mt-2 text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eum, aliquid, omnis laboriosam nemo corrupti eos velit necessitatibus cum, placeat ad ex maxime error? Quidem veritatis maxime optio tempore perspiciatis?</p>
+            <hr className="h-px my-6 bg-gray-700 border-0 dark:bg-gray-700"></hr>
+          </div>
         </div>
         <div>
           {" "}
